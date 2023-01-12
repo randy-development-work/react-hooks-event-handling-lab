2d559b2bbcbbb6f3093748e7f4ca4ec93ad733ed
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-01-12 19:59:04 +0300

    Completed Event Handling in React Lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+
+const EyesOnMe = () => {
+    const focusEvent = () => {
+        console.log('Good!');
+    }
+    const blurEvent = () => {
+        console.log('Hey! Eyes on me!');
+    }
+    return (
+        <>
+        <button onFocus={focusEvent} onBlur={blurEvent}>Eyes on me</button>
+        </>
+    )
+}
+
+export default EyesOnMe;
